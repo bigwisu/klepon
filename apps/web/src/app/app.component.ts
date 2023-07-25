be2159fb40d38087b8001e3c2edb371582826801
wisu@google.com <wisu@google.com>
2023-07-25 14:56:52 +0000

    app module

--- apps/web/src/app/app.component.ts
+++ apps/web/src/app/app.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
-import { NxWelcomeComponent } from './nx-welcome.component';
 
 @Component({
-  standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
   selector: 'klepon-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
+
 export class AppComponent {
   title = 'web';
 }
