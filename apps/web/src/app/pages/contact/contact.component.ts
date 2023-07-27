5f7d80a9f0f1f086c0bba1568ff9d1a466336053
wisu@google.com <wisu@google.com>
2023-07-27 14:12:59 +0000

    sample pages

---
+++ apps/web/src/app/pages/contact/contact.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'klepon-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.scss'],
+})
+export class ContactComponent {}
--- apps/web/src/app/pages/pages-routing.module.ts
+++ apps/web/src/app/pages/pages-routing.module.ts
@@ -3,13 +3,15 @@ import { RouterModule, Routes } from '@angular/router';
 import { PagesComponent } from './pages.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { FrontComponent } from './front/front.component';
+import { ContactComponent } from './contact/contact.component';
 
 const routes: Routes = [
   {
     path: '', component: PagesComponent,
     children: [
       { path: '', component: FrontComponent },
-      { path: 'about-us', component: AboutUsComponent }
+      { path: 'about-us', component: AboutUsComponent },
+      { path: 'contact', component: ContactComponent }
     ]
   }
 ];
