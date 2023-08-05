7ca83218a83068e0b52b418a757869471937b859
wisu@google.com <wisu@google.com>
2023-08-05 09:40:27 +0000

    clean-up contact form

--- apps/web/src/app/app-routing.module.ts
+++ apps/web/src/app/app-routing.module.ts
@@ -4,7 +4,8 @@ import { RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./pages/pages.module').then(m => m.PagesModule) },
   { path: 'app', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'login', redirectTo: 'auth', pathMatch: 'full' }
 ];
 
 @NgModule({
